8487511a271ee55a5b8553188b18df6337cd826c
Harris Borawski <harrisborawski@me.com>
2013-04-08 13:50:02 -0400

    Login changes
    The php pages that checks user existence will now check both managers
    and regular users for code reuse.
    
    Changed reference in main.html to reference new login page

--- check.php
+++ check.php
@@ -1,24 +1,40 @@
 <?php
 
 $mysql_host = "mysql4.000webhost.com";
-$mysql_database = "a7821088_items";
+$mysql_databaseAdmin = "a7821088_items";
+$mysql_databaseUsers = "";
 $mysql_user = "a7821088_manager";
 $mysql_password = "cupcakes1";
 
-$user = $_POST['username'];
+$user = $_POST['email'];
 $pass = $_POST['password'];
+$loginType = $_POST['type'];
 
 mysql_connect($mysql_host,$mysql_user,$mysql_password);
-@mysql_select_db($mysql_database);
-$query = "SELECT * FROM managers WHERE Username='$user' AND Password='$pass'";
-$result = mysql_query($query) or die("No such user");
+if ($loginType == 1){}
+	@mysql_select_db($mysql_databaseAdmin);
+	$query = "SELECT * FROM managers WHERE Username='$user' AND Password='$pass'";
+	$result = mysql_query($query) or die("No such user");
 
-$count = mysql_num_rows($result);
+	$count = mysql_num_rows($result);
 
-if($count == 1){
-	session_start();
-	$perm = true;
-	$_SESSION['permission'] = $perm;
+	if($count == 1){
+		session_start();
+		$perm = true;
+		$_SESSION['permission'] = $perm;
+	}
+}elseif($loginType==0){
+	@mysql_select_db($mysql_databaseUsers);
+	$query = "SELECT * FROM users WHERE Email='$email' AND Password='$pass'";
+	$result = mysql_query($query) or die("No such user");
+
+	$count = mysql_num_rows($result);
+
+	if($count ==1){
+		session_start();
+		$perm = true;
+		$_SESSION['user']= $perm;
+	}
 }
 
 header("location:main.html");
