5d0352d349ec94d4f496e47be2c387f3544be423
Harris Borawski <harrisborawski@me.com>
2013-04-08 17:53:02 -0400

    Database changes for new webhost
    Also seeing about using a single file for mysql info, not sure how well
    itll work

---
+++ CONST.php
@@ -0,0 +1,6 @@
+<?php
+	$mysql_host = "mysql3.000webhost.com";
+	$mysql_database = "a8711056_pics";
+	$mysql_user = "a8711056_manager";
+	$mysql_password = "cupcakes1";
+?>
--- check.php
+++ check.php
@@ -1,18 +1,13 @@
 <?php
 
-$mysql_host = "mysql4.000webhost.com";
-$mysql_databaseAdmin = "a7821088_items";
-$mysql_databaseUsers = "";
-$mysql_user = "a7821088_manager";
-$mysql_password = "cupcakes1";
-
+include 'CONST.php';
 $user = $_POST['email'];
 $pass = $_POST['password'];
 $loginType = $_POST['type'];
 
 mysql_connect($mysql_host,$mysql_user,$mysql_password);
 if ($loginType == 1){}
-	@mysql_select_db($mysql_databaseAdmin);
+	@mysql_select_db($mysql_database);
 	$query = "SELECT * FROM managers WHERE Username='$user' AND Password='$pass'";
 	$result = mysql_query($query) or die("No such user");
 
@@ -24,8 +19,8 @@ if ($loginType == 1){}
 		$_SESSION['permission'] = $perm;
 	}
 }elseif($loginType==0){
-	@mysql_select_db($mysql_databaseUsers);
-	$query = "SELECT * FROM users WHERE Email='$email' AND Password='$pass'";
+	@mysql_select_db($mysql_database);
+	$query = "SELECT * FROM users WHERE Email='$user' AND Password='$pass' and Confirmed='1'";
 	$result = mysql_query($query) or die("No such user");
 
 	$count = mysql_num_rows($result);
