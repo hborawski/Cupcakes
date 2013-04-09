84631393e09f10086fcc81552d23fc8540c4b07e
Harris Borawski <harrisborawski@me.com>
2013-04-08 20:29:24 -0400

    tried to parse the post data differently

--- check.php
+++ check.php
@@ -1,11 +1,12 @@
 <?php
 include 'CONST.php';
-$user = $_POST['email'];
-$pass = $_POST['password'];
-$loginType = $_POST['type'];
+
 
 $login = "incorrect";
 mysql_connect($mysql_host,$mysql_user,$mysql_password);
+$user = mysql_real_escape_string($_POST['email']);
+$pass = mysql_real_escape_string($_POST['password']);
+$loginType = mysql_real_escape_string($_POST['type']);
 if ($loginType == 1){
 	@mysql_select_db($mysql_database);
 	$query = "SELECT * FROM managers WHERE Username='$user' AND Password='$pass'";
@@ -21,7 +22,8 @@ if ($loginType == 1){
 	}
 }elseif($loginType==0){
 	@mysql_select_db($mysql_database);
-	$query = "SELECT * FROM users WHERE Email='$user' AND Password='$pass' AND Confirmed='1'";
+	$confirm = 1;
+	$query = "SELECT * FROM users WHERE Email='$user' AND Password='$pass'";
 	$result = mysql_query($query) or die("No such user");
 
 	$count = mysql_num_rows($result);
