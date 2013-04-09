8c5de161f0e960d4281ac87025ab89849a52e2e8
Harris Borawski <harrisborawski@me.com>
2013-04-08 23:18:59 -0400

    changed default insert bahavior

--- insert.php
+++ insert.php
@@ -18,7 +18,7 @@ move_uploaded_file($_FILES["upload"]["tmp_name"],
 
 mysql_connect($mysql_host,$mysql_user,$mysql_password);
 @mysql_select_db($mysql_database);
-$query = "INSERT INTO items (Name, Price, Available, SaleType, Quantity,  URL) VALUES('$name','$price','$avail',1,'$num','$url')";
+$query = "INSERT INTO items (Name, Price, Available, SaleType, Quantity,  URL) VALUES('$name','$price','$avail',0,'$num','$url')";
 echo $avail;
 mysql_query($query);
 
