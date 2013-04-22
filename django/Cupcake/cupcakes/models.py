04a75374c7dd6fbe1077e572176f973765fa4c12
Harris Borawski <harrisborawski@me.com>
2013-04-22 12:57:07 -0400

    Revert "Revert "preliminary model setup""
    This reverts commit 0a0e8fcf4baa87d84e8acae063c5e5e176b28b1f.

--- django/Cupcake/cupcakes/models.py
+++ django/Cupcake/cupcakes/models.py
@@ -4,11 +4,24 @@ from django.db import models
 
 class User(models.Model):
 	Email = models.CharField(max_length=15)
-	Password = models.CharField(max_length = 255)
+	Password = models.CharField(max_length=255)
 	Time = models.DateTimeField('date created')
 	Permission = models.IntegerField(default=0)
 	Location = models.IntegerField(default=0)
 	Salt = models.CharField(max_length=16)
 
+class Item(models.Model):
+	Name = models.CharField()
+	Price = models.DecimalField(max_digits=10,decimal_places=2)
+	Quantity = models.IntegerField(default=1)
+	Condition = models.integerField(default=0)
+	Description = models.CharField()
+	ForSale = models.IntegerField(default=1)
+	Available = models.IntegerField(default=1)
 
-
+class Notification(models.Model)
+	Recipient = models.Charfield(max_length=15)
+	Sender = models.CharField(max_length=15)
+	Message = models.CharField()
+	Time = model.DateTimeField()
+	Read = models.IntegerField(default=0)
